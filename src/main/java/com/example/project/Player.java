cb97865f0f6ed1603c7037b568a492f8191eaaa0
Eric Li <148896243+ericl339@users.noreply.github.com>
2025-02-28 17:33:30 +0000

    push

--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -16,21 +16,124 @@ public class Player{
     public ArrayList<Card> getAllCards(){return allCards;}
 
     public void addCard(Card c){
-        
+        hand.add(c);
     }
 
     public String playHand(ArrayList<Card> communityCards){      
-        return "Nothing";
+        allCards = new ArrayList<>(hand);
+        for (Card card1 : communityCards) {
+            allCards.add(card1);
+        }
+        sortAllCards();
+        String outcome = "Nothing";
+
+        ArrayList<Integer> rankF = findRankingFrequency();
+        ArrayList<Integer> suitF = findSuitFrequency();
+
+        if (highCard()) {
+            outcome = "High Card";
+        }
+        if (onePair()) {
+            outcome = "A Pair";
+        }
+        if (twoPair()) {
+            outcome = "Two Pair";
+        }
+        if (threeofAKind()) {
+            outcome = "Three of a Kind";
+        }
+        return outcome;
     }
 
-    public void sortAllCards(){} 
+    private boolean highCard() {
+        int highestValue = Integer.MIN_VALUE;
+        for (int i = 0; i < hand.size(); i ++) {
+            if (Utility.getRankValue(hand.get(i).getRank()) > highestValue) {
+                highestValue = Utility.getRankValue(hand.get(i).getRank());
+            }
+        }
+        if (Utility.getRankValue(allCards.get(allCards.size() - 1).getRank()) == highestValue) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean onePair() {
+        ArrayList<Integer> rankF = findRankingFrequency();
+        for (int i = 0; i < rankF.size(); i ++) {
+            if (rankF.get(i) > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean twoPair() {
+        ArrayList<Integer> rankF = findRankingFrequency();
+        int amountRepeat = 0;
+        for (int i = 0; i < rankF.size(); i ++) {
+            if (rankF.get(i) > 1) {
+                amountRepeat ++;
+            }
+        }
+        if (amountRepeat >= 2) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean threeofAKind() {
+        ArrayList<Integer> rankF = findRankingFrequency();
+        for (int i = 0; i < rankF.size(); i ++) {
+            if (rankF.get(i) > 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void sortAllCards(){
+        int minIndex = 0;
+        Card temp;
+        for (int i = 0; i < allCards.size(); i ++) {
+            minIndex = i;
+            for (int j = i; j < allCards.size(); j ++) {
+                if (Utility.getRankValue(allCards.get(j).getRank()) < Utility.getRankValue(allCards.get(minIndex).getRank())) {
+                    minIndex = j;
+                }
+            }
+            temp = allCards.get(i);
+            allCards.set(i, allCards.get(minIndex));
+            allCards.set(minIndex, temp);
+        }
+    } 
 
     public ArrayList<Integer> findRankingFrequency(){
-        return new ArrayList<>(); 
+        ArrayList<Integer> rankFrequency = new ArrayList<Integer>();
+        for (int i = 0; i < ranks.length; i ++) {
+            int count = 0;
+            for (int j = 0; j < allCards.size(); j ++) {
+                if (Utility.getRankValue(allCards.get(j).getRank()) == Utility.getRankValue(ranks[i])) {
+                    count ++;
+                }
+            }
+            rankFrequency.add(count);
+        }
+        return rankFrequency; 
     }
 
     public ArrayList<Integer> findSuitFrequency(){
-        return new ArrayList<>(); 
+        ArrayList<Integer> suitFrequency = new ArrayList<Integer>();
+        for (int i = 0; i < suits.length; i ++) {
+            int count = 0;
+            for (int j = 0; j < allCards.size(); j ++) {
+                if (allCards.get(j).getSuit().equals(suits[i])) {
+                    count ++;
+                }
+            }
+            suitFrequency.add(count);
+        }
+        return suitFrequency; 
     }
 
    
