f65c2093fd77cb5adb6749a88e86ce92f16ec4b4
Eric Li <148896243+ericl339@users.noreply.github.com>
2025-03-03 17:25:24 +0000

    push

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -3,38 +3,51 @@ import java.util.ArrayList;
 
 
 public class Game{
+    // compares the p1hand and p2 hand and see which one is greater
     public static String determineWinner(Player p1, Player p2,String p1Hand, String p2Hand,ArrayList<Card> communityCards){
+        // if p1 is greater than p2, p1 wins
         if (Utility.getHandRanking(p1Hand) > Utility.getHandRanking(p2Hand)) {
             return "Player 1 wins!";
         }
+        // else if p2 is greater, p2 wins
         else if (Utility.getHandRanking(p1Hand) < Utility.getHandRanking(p2Hand)) {
             return "Player 2 wins!";
         }
+        // if they tied then find the highest
         else if (Utility.getHandRanking(p1Hand) == Utility.getHandRanking(p2Hand)) {
+            // creates the highest for each player
             int player1High;
             int player2High;
+            // if the card at index 1 is higher than the card at index 0, set highest to index 1
             if (Utility.getRankValue(p1.getHand().get(1).getRank()) >= Utility.getRankValue(p1.getHand().get(0).getRank())) {
                 player1High = Utility.getRankValue(p1.getHand().get(1).getRank());
             }
+            // else sets it to index 0
             else {
                 player1High = Utility.getRankValue(p1.getHand().get(0).getRank());
             }
+            // if the card at index 1 is higher than the card at index 0, set highest to index 1
             if (Utility.getRankValue(p2.getHand().get(1).getRank()) >= Utility.getRankValue(p2.getHand().get(0).getRank())) {
                 player2High = Utility.getRankValue(p2.getHand().get(1).getRank());
             }
+            // else sets it to index 0
             else {
                 player2High = Utility.getRankValue(p2.getHand().get(0).getRank());
             }
+            // compares the two highest from each player
+            // if p1 is greater, p1 wins
             if (player1High > player2High) {
                 return "Player 1 wins!";
             }
+            // if p2 is greater, p2 wins
             else if (player1High < player2High) {
                 return "Player 2 wins!";
             }
         }
-
+        // returns a tie
         return "Tie!";
     }
+    // test code
     public static void main(String[] args) {
         Player player1 = new Player();
         Player player2 = new Player();
--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -99,7 +99,7 @@ public class Player{
         return false;
     }
 
-    // determines if hand has one pair
+    // determines if all cards have one pair
     private boolean onePair() {
         // creates a frequency list for rank
         ArrayList<Integer> rankF = findRankingFrequency();
@@ -114,152 +114,222 @@ public class Player{
         return false;
     }
 
+    // determines if all cards have two pairs
     private boolean twoPair() {
+        // creates a frequency list for rank
         ArrayList<Integer> rankF = findRankingFrequency();
+        // sets the amount repeating not zero to 0
         int amountRepeat = 0;
+        // iterates through the frequency list
         for (int i = 0; i < rankF.size(); i ++) {
+            // if the index has two cards of the same, add one to amount repeating
             if (rankF.get(i) > 1) {
                 amountRepeat ++;
             }
         }
+        // if there is at least two pairs, then return true. Else return false
         if (amountRepeat >= 2) {
             return true;
         }
         return false;
     }
 
+    // determines if all cards have three of a kind
     private boolean threeofAKind() {
+        // creates a rank frequency list
         ArrayList<Integer> rankF = findRankingFrequency();
+        // iterates through the rank frequency
         for (int i = 0; i < rankF.size(); i ++) {
+            // if there is 3 at the index, return true
             if (rankF.get(i) > 2) {
                 return true;
             }
         }
+        // return false
         return false;
     }
 
+    // determines if all cards have a straight
     private boolean straight() {
+        // creates a rank frequency list
         ArrayList<Integer> rankF = findRankingFrequency();
+        // initiates count to 0
         int count = 0;
+        // iterates through rank frequency
         for (int i = 0; i < rankF.size(); i ++) {
+            // if there is a card at the index, add one to count
             if (rankF.get(i) == 1) {
                 count ++;
             }
+            // else resets the counter
             else {
                 count = 0;
             }
+            // if count is 5, then there is a straight, return true
             if (count == 5) {
                 return true;
             }
         }
+        // returns false
         return false;
     }
 
+    // determines if all cards have a flush
     private boolean flush() {
+        // creates a list of suit frequency
         ArrayList<Integer> rankS = findSuitFrequency();
+        // iterates through the suit frequency
         for (int i = 0; i < rankS.size(); i ++) {
+            // determines if the int at the index is 5 and if it is, then return true because there can only be a max of 5 suits
             if (rankS.get(i) == 5) {
                 return true;
             }
         }
+        // return false
         return false;
     }
 
+    // determines if all cards have a full house
     private boolean fullHouse() {
+        // this works because all cards is sorted
+        // outer if statement passes if the first three are equal in rank
         if (allCards.get(0).getRank().equals(allCards.get(1).getRank()) && allCards.get(0).getRank().equals(allCards.get(2).getRank())) {
+            // sees the last two card and determines if they are the same
             if (allCards.get(3).getRank().equals(allCards.get(4).getRank())) {
                 return true;
             }
         }
+        // same concept but outer if now compares the first two and inner if compares the last three
         if (allCards.get(0).getRank().equals(allCards.get(1).getRank())) {
             if (allCards.get(2).getRank().equals(allCards.get(3).getRank()) && allCards.get(2).getRank().equals(allCards.get(4).getRank())) {
                 return true;
             }
         }
+        // returns false
         return false;
     }
 
+    // determines if all cards have a four of a kind
     private boolean fourOfAKind() {
+        // creates a frequency list for rank
         ArrayList<Integer> rankF = findRankingFrequency();
+        // iterates through the rank frequency
         for (int i = 0; i < rankF.size(); i ++) {
+            // if the int at the index is at least 4, return true
             if (rankF.get(i) > 3) {
                 return true;
             }
         }
+        // return false
         return false;
     }
 
+    // determines if all cards have a straight flush
     private boolean straightFlush(){
-        ArrayList<Integer> rankF = findRankingFrequency();
-        ArrayList<Integer> suitF = findSuitFrequency();
+        // calls flush to determine if all cards have a flush, and if so, goes to inner if
         if (flush()) {
+            // if all cards also has a straight, return true
             if (straight()) {
                 return true;
             }
         }
+        // return false
         return false;
     }
 
+    // determines if all cards have a royal flush
     private boolean royalFlush(){
+        // creates a frequecy rank list
         ArrayList<Integer> rankF = findRankingFrequency();
+        // calls flush to determine if all cards have a flush and if so, goes to inner if
         if (flush()) {
+            // initiates count to 0
             int count = 0;
+            // iterates through rank frequency starting at the card 10
             for (int i = 8; i < rankF.size(); i ++) {
+                // determines if there is a straight starting from 10
+                // adds count if there is a card
                 if (rankF.get(i) == 1) {
                     count ++;
                 }
+                // else resets
                 else {
                     count = 0;
                 }
+                // if count is 5 meaning there is a straight, return true
                 if (count == 5) {
                     return true;
                 }
             }
         }
+        // return false
         return false;
     }
-
+    // sorts card from low to high
     public void sortAllCards(){
+        // uses selection sort
+        // initiates min index and temp
         int minIndex = 0;
         Card temp;
+        // iterates through all cards
         for (int i = 0; i < allCards.size(); i ++) {
+            // sets min index to the index it's on
             minIndex = i;
+            // nested for loop
             for (int j = i; j < allCards.size(); j ++) {
+                // if the index j is less than min index, sets j to min index
                 if (Utility.getRankValue(allCards.get(j).getRank()) < Utility.getRankValue(allCards.get(minIndex).getRank())) {
                     minIndex = j;
                 }
             }
+            // swaps i and min index
             temp = allCards.get(i);
             allCards.set(i, allCards.get(minIndex));
             allCards.set(minIndex, temp);
         }
     } 
 
+    // finds the frequency list
     public ArrayList<Integer> findRankingFrequency(){
+        // creates the return array list
         ArrayList<Integer> rankFrequency = new ArrayList<Integer>();
+        // iterates through the ranks list
         for (int i = 0; i < ranks.length; i ++) {
+            // initiates count to 0
             int count = 0;
+            // nested for loop through all cards
             for (int j = 0; j < allCards.size(); j ++) {
+                // if the rank in all cards matches the rank is ranks list, count adds one
                 if (Utility.getRankValue(allCards.get(j).getRank()) == Utility.getRankValue(ranks[i])) {
                     count ++;
                 }
             }
+            // adds count to the return list
             rankFrequency.add(count);
         }
+        //returns the arraylist
         return rankFrequency; 
     }
 
+    //finds the frequency for suits
     public ArrayList<Integer> findSuitFrequency(){
+        // creates a return list
         ArrayList<Integer> suitFrequency = new ArrayList<Integer>();
+        // iterates through the suits list
         for (int i = 0; i < suits.length; i ++) {
+            // inititates count to 0
             int count = 0;
+            // nested for loop that goes through all cards
             for (int j = 0; j < allCards.size(); j ++) {
+                // if the suit at j matches the suit at i, count adds 1
                 if (allCards.get(j).getSuit().equals(suits[i])) {
                     count ++;
                 }
             }
+            // adds the count to frequency
             suitFrequency.add(count);
         }
+        // returns the frequency
         return suitFrequency; 
     }
 
